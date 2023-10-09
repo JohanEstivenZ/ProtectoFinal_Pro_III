2795d9eb438860239352dd1b4d12406cb1937c82
DarkBrotherV <juanjoserestrepomorales999@gmail.com>
2023-10-08 21:17:31 -0500

    Update ModelFactoryController.java

--- src/main/java/co/edu/uniquindio/subasta/subastasuq/controller/ModelFactoryController.java
+++ src/main/java/co/edu/uniquindio/subasta/subastasuq/controller/ModelFactoryController.java
@@ -3,5 +3,5 @@ package co.edu.uniquindio.subasta.subastasuq.controller;
 import co.edu.uniquindio.subasta.subastasuq.controller.service.IModelFactoryService;
 
 public class ModelFactoryController implements IModelFactoryService {
-    
+
 }
